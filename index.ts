9279fd5620413442b525fecd248de4959780c312
Ian Beddie <ian.beddie@mths.ca>
2024-09-29 08:43:33 +0000

    added working microwave program

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@
  *
  * @author  Ian Beddie
  * @version 1.0
- * @since 2024-09-25
+ * @since   2024-09-25
  */
 
 import { createPrompt } from 'bun-promptx'
@@ -12,6 +12,9 @@ const subTime = 60
 const pizzaTime = 45
 const soupTime = 105
 let cookTime = 0
+let minutes = 0
+let seconds = 0
+
 
 const userFood = createPrompt('Are you heating sub, pizza, or soup?:')
 
@@ -22,14 +25,29 @@ if (userFood.value == "sub") {
   } else if (userFood.value == "soup") {
     cookTime = soupTime
     } else {
-     console.log('you aint enter a food')
-    }
+      console.log('you aint enter a food')
+      }
 
 const foodAmount = createPrompt('How many or you heating?:')
 
-if 
+if (foodAmount.value == 1) {
+  cookTime = (cookTime * 1)
+} else if (foodAmount.value == 2) {
+  cookTime = (cookTime * 1.5)
+  } else if (foodAmount.value == 3) {
+    cookTime = (cookTime * 2)
+    } else {
+      console.log('wrong')
+      }
 
-console.log(`${cookTime} is cook time `)
+minutes = Math.floor(cookTime / 60);
+seconds = cookTime % 60;
+
+console.log(`Minutes: ${minutes}, Seconds: ${seconds}`);
+
+
+
+
 
 /*
     console.log(`${objectMass}kg of mass would produce ${energyOutput}J of energy. `)
